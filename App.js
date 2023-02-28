0fa264b2aa15576cffc619bac0e4aa3ee1e75130
looju <omofade2020@gmail.com>
2023-02-28 08:40:18 +0000

    preloading fonts

--- App.js
+++ App.js
@@ -1,13 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Oswald_400Regular,
+  Lato_400Regular,
+  Anton_400Regular,
+  Griffy_400Regular,
+  Tangerine_400Regular,
+  Arizonia_400Regular,
+  BebasNeue_400Regular
+} from "@expo-google-fonts/dev";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+  const [fontsLoaded] = useFonts({
+    Oswald_400Regular,
+    Anton_400Regular,
+    BebasNeue_400Regular,
+    Tangerine_400Regular,
+    Lato_400Regular
+  });
+
+  if (!fontsLoaded){
+    return null
+  }
+
+  else{
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    )
+  }
+ 
 }
 
 const styles = StyleSheet.create({
