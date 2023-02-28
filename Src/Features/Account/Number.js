d0f25f41fdd6f7da2c9562b3d1fe3499e2ca56b8
looju <omofade2020@gmail.com>
2023-02-28 13:10:11 +0000

    building number screen

--- Src/Features/Account/Number.js
+++ Src/Features/Account/Number.js
@@ -1,12 +1,62 @@
-import { View, Text,StyleSheet } from 'react-native'
-import { Button, TextInput } from 'react-native-paper'
-import React from 'react'
+import { View, Text, StyleSheet, Dimensions, TextInput } from "react-native";
+import { Button } from "react-native-paper";
+import React from "react";
 
 export const Number = () => {
   return (
-    <View>
-      <Text>Number</Text>
+    <View style={Styles.container}>
+      <View style={Styles.textView}>
+        <Text style={Styles.text}>Enter your phone number</Text>
+      </View>
+      <View style={Styles.inputContainer}>
+        <View style={Styles.inputView}>
+          <TextInput
+            label="Email"
+            value={"+234"}
+            editable={false}
+            underlineColorAndroid="#BF40BF"
+            textAlign="center"
+            readOnly={true}
+            style={{ color: "#fff" }}
+          />
+        </View>
+        <View>
+        <TextInput
+            label="Email"
+            value={"+234"}
+            editable={false}
+            underlineColorAndroid="#BF40BF"
+            textAlign="center"
+            readOnly={true}
+            style={{ color: "#fff" }}
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
 
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0,0.95)",
+  },
+  textView: {
+    marginTop: 25,
+  },
+  text: {
+    color: "#fff",
+  },
+  inputContainer: {
+    marginTop: 120,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  inputView: {
+    flex: 0.2,
+    marginRight: 10,
+  },
+  fullInputView:{
+    
+  }
+});
