b28ff041e422dc34a574c986939795407f86164f
looju <omofade2020@gmail.com>
2023-03-02 22:54:55 +0000

    Fetching current location

--- Src/Features/App/HomeScreen.js
+++ Src/Features/App/HomeScreen.js
@@ -1,8 +1,28 @@
 import { View, Text, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
+import * as Location from "expo-location";
 
 export const HomeScreen = () => {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        console.log("permission denied")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.Balanced,
+      });
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={Styles.container}>
       <MapView
